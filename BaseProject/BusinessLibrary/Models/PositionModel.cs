14a07f5422aea55b7834b9640371d6c50d0b7326
Omar <rogeromar11@hotmail.com>
2018-09-10 12:34:08 -0600

    Se agrego la carpeta Modelos

---
+++ BaseProject/BusinessLibrary/Models/PositionModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLibrary.Models
+{
+    public class PositionModel
+    {
+        public int idPosition;
+        public string positionName;
+        public int idSalary;
+
+        public int IdPosition { get => idPosition; set => idPosition = value; }
+        public string PositionName { get => positionName; set => positionName = value; }
+        public int IdSalary { get => idSalary; set => idSalary = value; }
+    }
+}
