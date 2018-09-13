3dea98f3ec98ccac1de4c05c24dedc0928fb8149
Omar <rogeromar11@hotmail.com>
2018-09-13 13:29:34 -0600

    Se agrego el idProduct

--- BaseProject/BusinessLibrary/Models/ExitInvoiceModel.cs
+++ BaseProject/BusinessLibrary/Models/ExitInvoiceModel.cs
@@ -8,13 +8,13 @@ namespace BusinessLibrary.Models
 {
     public class ExitInvoiceModel
     {
-        public int idInputInovice;
+        public int idInputInvoice;
         public int idBusiness;
         public List<ProductModel> products;
         public string date;
         public string destiny;
 
-        public int IdInputInovice { get => idInputInovice; set => idInputInovice = value; }
+        public int IdInputInovice { get => idInputInvoice; set => idInputInvoice = value; }
         public int IdBusiness { get => idBusiness; set => idBusiness = value; }
         public List<ProductModel> Products { get => products; set => products = value; }
         public string Date { get => date; set => date = value; }
