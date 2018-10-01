4a11a7daedc85bc70ad8a7581c63a258273432dd
Saul Reyes <sauldf93@hotmail.com>
2018-10-01 12:07:57 -0600

    Se cambia todo lo de factura entrada y salida

---
+++ BaseProject/BusinessLibrary/Models/InvoiceInputExitModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLibrary.Models
+{
+    public class InvoiceInputExitModel
+    {
+        public int idNumInvoice;
+        public int idProduct;
+
+        public int IdNumInvoice { get => idNumInvoice; set => idNumInvoice = value; }
+        public int IdProduct { get => idProduct; set => idProduct = value; }
+    }
+}
---
+++ BaseProject/DataBaseLibrary/DBInputExitInvoice.cs
@@ -0,0 +1,93 @@
+﻿using BusinessLibrary.Models;
+using Dapper;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseLibrary
+{
+    public class DBInputExitInvoice
+    {
+        private static String LoadConnectionString(String id = "default")
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+
+        public static bool InsertInputInvoice(InputExitDetaillsModel inputInvoice)
+        {
+            try
+            {
+                using (IDbConnection cnn = new MySqlConnection(LoadConnectionString()))
+                {
+                    cnn.Execute("INSERT INTO DetailInvoice (idEmployee, typeTransaction, Provider)" +
+                        " VALUES(@idEmployee, @typeTransaction, @Provider)", inputInvoice);
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        public static bool InsertExitInvoice(InputExitDetaillsModel inputInvoice)
+        {
+            try
+            {
+                using (IDbConnection cnn = new MySqlConnection(LoadConnectionString()))
+                {
+                    cnn.Execute("INSERT INTO DetailInvoice (idEmployee, typeTransaction, Destiny)" +
+                        " VALUES(@idEmployee, @typeTransaction, @Destiny)", inputInvoice);
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        public static List<InputExitDetaillsModel> SelectAllInputInvoice(InputExitDetaillsModel inputInvoice)
+        {
+            using (IDbConnection cnn = new MySqlConnection(LoadConnectionString()))
+            {
+                var output = cnn.Query<InputExitDetaillsModel>("SELECT * FROM DetailInvoice WHERE typeTransaction = @Entrada", inputInvoice);
+                return output.ToList();
+            }
+        }
+
+        public static List<InputExitDetaillsModel> SelectAllExitInvoice(InputExitDetaillsModel inputInvoice)
+        {
+            using (IDbConnection cnn = new MySqlConnection(LoadConnectionString()))
+            {
+                var output = cnn.Query<InputExitDetaillsModel>("SELECT * FROM DetailInvoice WHERE typeTransaction = @Salida", inputInvoice);
+                return output.ToList();
+            }
+        }
+
+        public static InputExitDetaillsModel SelectInputExitInvoiceByIdNum(InputExitDetaillsModel IdNum)
+        {
+            using (IDbConnection cnn = new MySqlConnection(LoadConnectionString()))
+            {
+                var output = cnn.Query<InputExitDetaillsModel>("SELECT * FROM DetailInvoice WHERE idNumInvoice = @idNumInvoice", IdNum);
+                return output.Single();
+            }
+        }
+
+        public static List<InputExitDetaillsModel> SelectInputExitInvoiceByType(InputExitDetaillsModel inputInvoice)
+        {
+            using (IDbConnection cnn = new MySqlConnection(LoadConnectionString()))
+            {
+                var output = cnn.Query<InputExitDetaillsModel>("SELECT * FROM DetailInvoice WHERE typeTransaction = @typeTransaction", inputInvoice);
+                return output.ToList();
+            }
+        }
+    }
+}
